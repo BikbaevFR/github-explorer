3d33c5b28f1753bc7b5af0c889535cbd697cc434
Cass <bikbaevf18@gmail.com>
2022-12-18 15:16:53 +0400

    Update Avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,19 +1,17 @@
 import { Container } from "@components/Avatar/styles";
+import { ReactComponent as UserSVG } from "@static/img/user.svg";
 import { FC } from "react";
 import "twin.macro";
-
-const defaultUrl =
-  "https://st3.depositphotos.com/9998432/13335/v/600/depositphotos_133352156-stock-illustration-default-placeholder-profile-icon.jpg";
 
 interface Props {
   url?: string;
   size?: string;
 }
 
-const Avatar: FC<Props> = ({ url = defaultUrl, size = "" }) => {
+const Avatar: FC<Props> = ({ url, size = "" }) => {
   return (
     <Container size={size}>
-      <img src={url} alt="avatar" />
+      {url ? <img src={url} alt="avatar" /> : <UserSVG />}
     </Container>
   );
 };
